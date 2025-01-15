7587f65d39e239c1d2cb5164670583cda402cf2a
Efe <61231517+aefecoban@users.noreply.github.com>
2025-01-16 02:24:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = async (req, res) => {
     const ctx = App.app.createContext(req, res);
     
     // Call Koa's handleRequest method to process the request
-    await App.app.handleRequest(ctx.req, ctx.res);
+    await App.app.callback()(ctx.req, ctx.res);
   } catch (err) {
     console.error('Error handling request:', err);
     res.status(500).send('Internal Server Error');
