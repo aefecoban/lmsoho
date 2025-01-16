c646037b6338735e2c3bc7c061a67e11cb7c08d8
Efe <61231517+aefecoban@users.noreply.github.com>
2025-01-16 04:19:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 const ApplicationClass = require("./Application");
 const App = new ApplicationClass();
-
-module.exports = async (req, res) => {
-  try {
-    // Create a Koa context for the request and response
-    const ctx = App.app.createContext(req, res);
-    
-    // Call Koa's handleRequest method to process the request
-    await App.app.callback()(ctx.req, ctx.res);
-  } catch (err) {
-    console.error('Error handling request:', err);
-    res.status(500).send('Internal Server Error');
-  }
-};
+App.Start();
